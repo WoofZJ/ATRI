b5b7b35382b402399b33533a84f1492229fa346c
WoofZJ <zjwoof@gmail.com>
2025-12-04 23:25:03 +0800

    feat(Test): add tests for MetaEvent serialization
    添加了对元事件Json解析的测试

---
+++ ATRI.Test/SerializerTests/MetaEventTests.cs
@@ -0,0 +1,57 @@
+﻿using ATRI.OneBot.Events;
+using ATRI.OneBot.JsonConverters;
+
+namespace ATRI.Test.SerializerTests;
+
+public class MetaEventTests
+{
+    [Fact]
+    public void LifeCycleEventTest()
+    {
+        var json = """
+            {"time":1764859724,"self_id":10000,"post_type":"meta_event","meta_event_type":"lifecycle","sub_type":"connect"}
+            """;
+        var evt = OneBotSerializer.Deserialize<Event>(json);
+        Assert.Equal(typeof(LifecycleEvent), evt?.GetType());
+        Assert.Equal(evt, new LifecycleEvent
+        {
+            Time = 1764859724,
+            SelfId = 10000,
+            PostType = "meta_event",
+            MetaEventType = "lifecycle",
+            SubType = "connect"
+        });
+    }
+
+    [Fact]
+    public void HeartbeatEventTest()
+    {
+        var json = """
+            {"time":1764860804,"self_id":10000,"post_type":"meta_event","meta_event_type":"heartbeat","status":{"online":true,"good":true},"interval":60000}
+            """;
+        var evt = OneBotSerializer.Deserialize<Event>(json);
+        Assert.Equal(typeof(HeartbeatEvent), evt?.GetType());
+        Assert.Equal(evt, new HeartbeatEvent
+        {
+            Time = 1764860804,
+            SelfId = 10000,
+            PostType = "meta_event",
+            MetaEventType = "heartbeat",
+            Status = new HeartbeatEvent.HeartbeatStatus
+            {
+                Online = true,
+                Good = true
+            },
+            Interval = 60000
+        });
+    }
+
+    [Fact]
+    public void UnknownMetaEventTypeTest()
+    {
+        var json = """
+            {"time":1764859724,"self_id":10000,"post_type":"meta_event","meta_event_type":"unknown_type"}
+            """;
+        Assert.Throws<NotSupportedException>(() => OneBotSerializer.Deserialize<Event>(json));
+    }
+}
